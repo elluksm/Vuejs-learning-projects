02db5ae273b5a6bba4ea8b24983ffd401f9e317e
Elina <elluksm@gmail.com>
2021-11-09 10:05:08 +0200

    basic events project

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: ''
     };
   },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce() {
+      this.counter--;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName ;
+    },
+    submitForm() {
+      alert("Submitted!");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    }
+  }
 });
 
 app.mount('#events');
